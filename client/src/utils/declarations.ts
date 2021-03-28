56ed39f962dfd8de63af07209d4bf0e9408b7f22
Felicia Schnell <felicia.schnell@hotmail.com>
2021-03-28 13:25:07 +0200

    Full stack 3
    Add new export type for tasks.

--- client/src/utils/declarations.ts
+++ client/src/utils/declarations.ts
@@ -2,4 +2,12 @@ export type Todo = {
     description: string;
     id: number;
     title: string;
+    tasks: Task[];
+}
+export type Task = {
+    id: number;
+    todoId: number;
+    memberId: number;
+    name: string;
+    completed: boolean;
 }
