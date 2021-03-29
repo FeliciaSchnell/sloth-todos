5c1b1d1ae28aa34625932f7ca80def85c0992a09
Felicia Schnell <felicia.schnell@hotmail.com>
2021-03-29 18:28:08 +0200

    Full stack 3
    Show task list in gui and enable creating and deleting new list items

--- client/src/components/TaskList.vue
+++ client/src/components/TaskList.vue
@@ -9,7 +9,7 @@
     <div class="listItem" v-for="(item, index) in data" :key="index">
       <input class="checkbox" type="checkbox" v-model="item.completed" :checked="item.completed">
       <input class="taskName" type="text" v-model="item.name" placeholder="Title">
-      <svg height="24" width="24" viewBox="0 0 24 24" class="deleteIcon" title="Delete task" @click="$emit('taskDelete')"><path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z" fill="#000"/></svg>
+      <svg height="24" width="24" viewBox="0 0 24 24" class="deleteIcon" title="Delete task" @click="$emit('taskDelete', index)"><path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z" fill="#000"/></svg>
     </div>
   </div>
 </template>
--- client/src/components/Todo.vue
+++ client/src/components/Todo.vue
@@ -2,17 +2,39 @@
   <div class="inputsection">
       <input class="title" type="text" v-model="item.title" placeholder="Title" @keypress="$emit('update')" />
       <textarea class="description" v-model="item.description" placeholder="Description" @keypress="$emit('update')"></textarea>
+      <TaskList :data="item.tasks" v-on:newTask="newTask" v-on:taskUpdate="$emit('update')" v-on:taskDelete="deleteTask($event)" />
   </div>
 </template>
 
 <script lang="ts">
 import { Todo } from '@/utils/declarations';
 import { Component, Prop, Vue } from 'vue-property-decorator';
-
-@Component
+import TaskList from '@/components/TaskList.vue';
+@Component({
+  components: {
+    TaskList
+  },
+})
 export default class InputSection extends Vue {
     @Prop() item!: Todo;
-}
+
+    newTask() {
+        console.log('newTask');
+        this.item.tasks.push(
+        {
+            id: -1,
+            todoId: -1,
+            memberId: -1,
+            name: "",
+            completed: false,
+        });
+    }
+    deleteTask(index: number) {
+        console.log('deleteTask');
+        this.item.tasks.splice(index, 1);
+        
+    }
+}   
 </script>
 
 <style scoped lang="scss">
@@ -29,7 +51,7 @@ export default class InputSection extends Vue {
         font-size: 36px;
     }
     .description {
-        flex: 1;
+        /* flex: 1; */
         border: none;
         padding: $l $xxl ($xxl * 2)  0 ;
     }
